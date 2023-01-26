5240e8416b364ec57be92da8c065625ece9542d0
amantur545 <amantur11@gmail.com>
2023-01-26 12:13:12 +0600

    new orm

---
+++ main.py
@@ -0,0 +1,36 @@
+# file = open('test1.txt','r')
+# # file.write('Hello\n')
+# file.writelines("['Hello','John']")
+# # file.write('World')
+# file.write('Hello')
+# f = file.read()
+
+# print(file.readline())
+
+# file.close()
+
+
+# with open('test1.txt') as file:
+#     print(file.read())
+
+
+# JSON
+ 
+# сериализация (с питона в json) -> dump, dumps
+# десерилизация (с  json в питон) ->  load, loads
+
+# import json
+# d = {'hello': True, 'age': 2, 'name': None}
+# json_obj = json.dumps(d)
+# print(json_obj)
+# python_obj = json.loads(json_obj)
+# print(python_obj)
+
+
+# import json
+# with open('data.json','r') as file:
+    # python_obj = json.loads(file.read()) 
+    # python_obj = json.load('file')
+    # print(python_obj)
+
+# OOП ,GIT, PostgresSQL,  (htpp, orm)
